7bd9193be223cbae5208b548f566bffd920c9f7c
MarttenKaja <kaja.martten@gmail.com>
2026-01-06 13:48:18 +0200

    laulu näitamine on lisatud

--- conf.php
+++ conf.php
@@ -2,6 +2,6 @@
 $serverinimi='localhost';
 $kasutajanimi='opilaneMartten';
 $parool='12345';
-$andmebaasinimi='';
+$andmebaasinimi='haal';
 $yhendus=new mysqli($serverinimi, $kasutajanimi, $parool, $andmebaasinimi);
 $yhendus->set_charset("utf8");
