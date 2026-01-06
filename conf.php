3be65e15da5b1c253d38d647efde4bca3c46bade
IrinaMerkulova <irina.merkulova@gmail.com>
2026-01-06 12:25:00 +0200

    alusfailid on lisatud

---
+++ conf.php
@@ -0,0 +1,7 @@
+<?php
+$serverinimi='localhost';
+$kasutajanimi='';
+$parool='12345';
+$andmebaasinimi='';
+$yhendus=new mysqli($serverinimi, $kasutajanimi, $parool, $andmebaasinimi);
+$yhendus->set_charset("utf8");
