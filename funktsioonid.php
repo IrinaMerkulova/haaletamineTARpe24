57aebd0d7e0b33c7dffcaca8639649ddae792d19
Timur Grechishkin <1ame.xyz@gmail.com>
2026-01-06 14:16:06 +0200

    funktsioonid on loodud - lisas funktsioonid.php - lisas haaletamineFunktsioonidega.php - lisas funktsiooni kutsumine ja lisamisfunktsioon

---
+++ funktsioonid.php
@@ -0,0 +1,53 @@
+<?php
+require('conf.php');
+// tabeli sisu kovamise funktsioon
+
+function kuvaTabeliLaulud(){
+    global $yhendus;
+
+    $paring = $yhendus->prepare(
+        "SELECT id, lauluNimi, laulja, pilt, punktid, lisamisaeg
+     FROM laulud
+     WHERE avalik = 1"
+    );
+    $paring->bind_result(
+        $id, $lauluNimi, $laulja, $pilt, $punktid, $lisamisaeg
+    );
+    $paring->execute();
+
+    while ($paring->fetch()) {
+        echo "<tr>";
+        echo "<td>" . htmlspecialchars($lauluNimi) . "</td>";
+        echo "<td>" . htmlspecialchars($laulja) . "</td>";
+        echo "<td><img src='" . htmlspecialchars($pilt) . "' alt='pilt'></td>";
+        echo "<td>$punktid</td>";
+        echo "<td>$lisamisaeg</td>";
+        echo "<td><a href='?lisa1punkt=$id'>+1 punkt</a></td>";
+        echo "</tr>";
+    }
+}
+
+// punkti lisamise funktsioon
+function lisa1punkt($id){
+    global $yhendus;
+
+    $paring = $yhendus->prepare(
+        "UPDATE laulud SET punktid = punktid + 1 WHERE id = ?"
+    );
+    $paring->bind_param('i', $id);
+    $paring->execute();
+}
+
+function lauluLisamine($lauluNimi, $laulja, $pilt){
+    global $yhendus;
+
+    $paring = $yhendus->prepare(
+        "INSERT INTO laulud (lauluNimi, laulja, pilt, avalik, lisamisaeg)
+        VALUES (?, ?, ?, 1, NOW())"
+    );
+    $paring->bind_param('sss', $lauluNimi, $laulja, $pilt);
+    $paring->execute();
+}
+
+
+
---
+++ haaletamineFunktsioonidega.php
@@ -0,0 +1,58 @@
+<?php
+require ('funktsioonid.php');
+//funktsiooni kutsumine
+if(isset($_REQUEST['lisa1punkt'])){
+    lisa1punkt($_REQUEST['lisa1punkt']);
+    header("Location: $_SERVER[PHP_SELF]");
+    exit();
+}
+
+// kutsume lisamisfunktsiooni
+if(!empty($_REQUEST['lauluNimi'])){
+    lauluLisamine($_REQUEST['lauluNimi'], $_REQUEST['laulja'], $_REQUEST['pilt']);
+    header("Location: $_SERVER[PHP_SELF]");
+    exit();
+}
+
+?>
+<!DOCTYPE html>
+<html lang="et">
+<head>
+    <meta charset="UTF-8">
+    <title>Laulude leht</title>
+
+</head>
+<body>
+
+<h1>🎵 Laulude hääletus (funktsioonid on eraldi php failis)</h1>
+
+<table>
+    <tr>
+        <th>Laulu nimi</th>
+        <th>Laulja</th>
+        <th>Pilt</th>
+        <th>Punktid</th>
+        <th>Lisamisaeg</th>
+        <th>+1 punkt</th>
+    </tr>
+    <?php
+    kuvaTabeliLaulud();
+    ?>
+</table>
+
+<h2>Lisa uus laul</h2>
+<form action="?" method="post">
+    <label>Laulu nimi:</label><br>
+    <input type="text" name="lauluNimi"><br><br>
+
+    <label>Laulja:</label><br>
+    <input type="text" name="laulja"><br><br>
+
+    <label>Pildi URL:</label><br>
+    <textarea name="pilt"></textarea><br><br>
+
+    <input type="submit" value="Lisa laul">
+</form>
+
+</body>
+</html>
