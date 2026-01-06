b919c09c9931d193081b1132b43f9c6619281356
Kaur Pakaste <kaur.pakaste@gmail.com>
2026-01-06 13:25:09 +0200

    Parandasin confi ja sqlKood

--- conf.php
+++ conf.php
@@ -2,6 +2,6 @@
 $serverinimi='localhost';
 $kasutajanimi='opilaneKaur';
 $parool='12345';
-$andmebaasinimi='';
+$andmebaasinimi='haal';
 $yhendus=new mysqli($serverinimi, $kasutajanimi, $parool, $andmebaasinimi);
 $yhendus->set_charset("utf8");
