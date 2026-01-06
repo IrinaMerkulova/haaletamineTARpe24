ee4e4efbec34c1c1e41ee08f8e4173e17b94eea4
Saimon Siipan <Saimon.Siipan@gmail.com>
2026-01-06 14:54:52 +0200

    tegin miinus - punkt ja nyyd kõik punktid lähevad nulli

--- funktsioonid.php
+++ funktsioonid.php
@@ -20,13 +20,94 @@ function kuvaTabeliLaulud()
         echo "<tr>";
         echo "<td>" . htmlspecialchars($lauluNimi) . "</td>";
         echo "<td>" . htmlspecialchars($laulja) . "</td>";
-        echo "<td><img src='" . htmlspecialchars($pilt) . "'></td>";
+        echo "<td><img src='" . htmlspecialchars($pilt) . "'alt='pilt'></td>";
         echo "<td>$punktid</td>";
         echo "<td>$lisamisaeg</td>";
         echo "<td><a href='?lisa1punkt=$id'>+1 punkt</a></td>";
+        echo "<td><a href='?kustuta=$id'>kustuta</a></td>";
+        echo "<td><a href='?eemalda1puntk=$id'>-1 punkt</a></td>";
+        echo "<td><a href='?nullpunkt=$id'>0 punkt</a></td>";
+
+
         echo "</tr>";
     }
+    if (isset($_REQUEST['lisa1punkt'])) {
+        $paring = $yhendus->prepare(
+            "UPDATE laulud SET punktid = punktid + 1 WHERE id = ?"
+        );
+        $paring->bind_param('i', $_REQUEST['lisa1punkt']);
+        $paring->execute();
+        header("Location: " . $_SERVER['PHP_SELF']);
+        exit;
+    }
+    if (isset($_REQUEST['eemalda1puntk'])) {
+        $paring = $yhendus->prepare(
+            "UPDATE laulud SET punktid = punktid - 1 WHERE id = ?"
+        );
+        $paring->bind_param('i', $_REQUEST['lisa1punkt']);
+        $paring->execute();
+        header("Location: " . $_SERVER['PHP_SELF']);
+        exit;
+    }
+
 }
+//punkti lisamise funktsioon
+function lisa1puntk($id)
+{
+    global $yhendus;
+    $paring = $yhendus->prepare(
+        "Update laulud SET punktid = punktid + 1 WHERE id = ?"
+    );
+    $paring->bind_param('i', $id);
+    $paring->execute();
+}
+//laulu kustutamine
+function laulukustutamine($id)
+{
+    global $yhendus;
+    $paring = $yhendus->prepare(
+        "DELETE From laulud Where id = ?"
+    );
+    $paring->bind_param('i', $id);
+    $paring->execute();
+    }
+//laulu lisamine
+    function laululisamine($laulunimi, $laulja, $pilt)
+    {
+        global $yhendus;
+        $paring = $yhendus->prepare(
+            "INSERT INTO laulud (lauluNimi, laulja, pilt, avalik, lisamisaeg)
+         VALUES (?, ?, ?, 1, NOW())"
+        );
+        $paring->bind_param(
+            'sss', $laulunimi, $laulja, $pilt);
+
+        $paring->execute();
+
+}
+//punktid eemaladime
+function eemalda1puntk($id)
+{
+    global $yhendus;
+    $paring = $yhendus->prepare(
+        "Update laulud SET punktid = punktid -1 WHERE id = ?"
+    );
+    $paring->bind_param('i', $id);
+    $paring->execute();
+}
+function nullpunkt($id)
+{
+    global $yhendus;
+    $paring = $yhendus->prepare(
+        "Update laulud SET punktid = 0 WHERE id = ?"
+    );
+    $paring->bind_param('i', $id);
+    $paring->execute();
+}
+
+
+
+
 
 
 
--- haaletamineFunktsioon.php
+++ haaletamineFunktsioon.php
@@ -1,5 +1,37 @@
 <?php
 require ('funktsioonid.php');
+//kutsume laulukustutamine
+if (isset($_REQUEST['kustuta'])) {
+    laulukustutamine($_REQUEST['kustuta']);
+    header("Location: " . $_SERVER['PHP_SELF']);
+    exit();
+}
+//funktsiooni kutsumine
+if (isset($_REQUEST['lisa1punkt'])) {
+    lisa1puntk($_REQUEST['lisa1punkt']);
+    header("Location: " . $_SERVER['PHP_SELF']);
+    exit();
+}
+
+//kutsume lisamis funktsiooni
+if (!empty($_REQUEST['lauluNimi'])) {
+    laululisamine(
+        $_REQUEST['lauluNimi'], $_REQUEST['laulja'], $_REQUEST['pilt']);
+            header("Location: " . $_SERVER['PHP_SELF']);
+    exit();
+
+}
+if (isset($_REQUEST['eemalda1puntk'])) {
+    eemalda1puntk($_REQUEST['eemalda1puntk']);
+    header("Location: " . $_SERVER['PHP_SELF']);
+    exit();
+}
+if (isset($_REQUEST['nullpunkt'])) {
+    nullpunkt($_REQUEST['nullpunkt']);
+    header("Location: " . $_SERVER['PHP_SELF']);
+    exit();
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="et">
@@ -20,10 +52,30 @@ require ('funktsioonid.php');
         <th>Punktid</th>
         <th>Lisamisaeg</th>
         <th>+1 punkt</th>
+        <th>kustuta</th>
+        <th>-1 punkt</th>
+        <th>punktid 0</th>
+
     </tr>
     <?php
     kuvaTabeliLaulud();
     ?>
 </table>
+<h2>Lisa uus laul</h2>
+<form action="?" method="post">
+    <label>Laulu nimi:</label><br>
+    <input type="text" name="lauluNimi"><br><br>
+
+    <label>Laulja:</label><br>
+    <input type="text" name="laulja"><br><br>
+
+    <label>Pildi URL:</label><br>
+    <textarea name="pilt"></textarea><br><br>
+
+    <input type="submit" value="Lisa laul">
+</form>
+
+</body>
+</html>
 </body>
 </html>
