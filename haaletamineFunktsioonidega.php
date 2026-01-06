c5b2731c2abc3e5d5ad81e9569278094b6fd7f6f
Sten Uesson <StenUesson@gmail.com>
2026-01-06 14:55:04 +0200

    tehtud ilusaks ja kustutamise, punkti lisamise funktsioonid oli lisatud

--- haaletamineFunktsioonidega.php
+++ haaletamineFunktsioonidega.php
@@ -1,11 +1,33 @@
 <?php
 require ('funktsioonid.php');
+//kutsume lauluKustutamine
+if (isset($_REQUEST['kustuta'])) {
+    lauluKustutamine($_REQUEST['kustuta']);
+    header("Location: " . $_SERVER['PHP_SELF']);
+    exit();
+}
+
+//funktsiooni kutsumine
+if (isset($_REQUEST['lisa1punkt'])) {
+    lisa1punkt($_REQUEST['lisa1punkt']);
+    header("Location: " . $_SERVER['PHP_SELF']);
+    exit();
+}
+
+//kutsume lisamisfunktsioonid
+if(!empty($_REQUEST['lauluNimi'])){
+    lauluLisamine(
+        $_REQUEST['lauluNimi'], $_REQUEST['laulja'], $_REQUEST['pilt']);
+    header("Location: " . $_SERVER['PHP_SELF']);
+    exit();
+}
 ?>
 <!doctype html>
 <html lang="et">
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 <h1> Laulude hääletus (funktsioonid on realdi php failis)</h1>
@@ -23,5 +45,18 @@ require ('funktsioonid.php');
     kuvaTabeliLaulud();
     ?>
 </table>
+<h2>Lisa uus laul</h2>
+<form action="?" method="post">
+    <label>Laulu nimi:</label><br>
+    <input type="text" name="lauluNimi"><br><br>
+
+    <label>Laulja:</label><br>
+    <input type="text" name="laulja"><br><br>
+
+    <label>Pildi URL:</label><br>
+    <textarea name="pilt"></textarea><br><br>
+
+    <input type="submit" value="Lisa laul">
+</form>
 </body>
 </html>
