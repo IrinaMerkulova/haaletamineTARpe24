ef73bae28b51812d5536dd472bc6a40e040e7293
Polina Gussarova <Polina.Gussarova@gmail.com>
2026-01-06 12:58:14 +0200

    kasutajanimi on lisatud

--- conf.php
+++ conf.php
@@ -1,6 +1,6 @@
 <?php
 $serverinimi='localhost';
-$kasutajanimi='';
+$kasutajanimi='opilaneRasmus';
 $parool='12345';
 $andmebaasinimi='';
 $yhendus=new mysqli($serverinimi, $kasutajanimi, $parool, $andmebaasinimi);
