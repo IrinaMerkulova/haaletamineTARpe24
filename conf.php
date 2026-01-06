af30808d47d759a33f52a6fc29e995a342e58748
Danil Adamiv <Daniladamiv@gmail.com>
2026-01-06 12:58:19 +0200

    Kasutajanimi on lisatud

--- conf.php
+++ conf.php
@@ -1,6 +1,6 @@
 <?php
 $serverinimi='localhost';
-$kasutajanimi='';
+$kasutajanimi='opilaneSavva';
 $parool='12345';
 $andmebaasinimi='';
 $yhendus=new mysqli($serverinimi, $kasutajanimi, $parool, $andmebaasinimi);
