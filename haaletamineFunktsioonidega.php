9f5f90f76de3822d02d5c2630f9035b1a3933b5f
Siim-byte <beloimiis@gmail.com>
2026-01-09 17:13:06 +0200

    kuvaTabeli funktsioon on loodud

---
+++ haaletamineFunktsioonidega.php
@@ -0,0 +1,28 @@
+<?php
+require ('funktsioonid.php');
+?>
+<!DOCTYPE html>
+<html lang="et">
+<head>
+    <meta charset="UTF-8">
+    <title>Laulude leht</title>
+</head>
+<body>
+
+<h1>🎵 Laulude hääletus (funktsioonid on eraldi php failis)</h1>
+
+<table>
+    <tr>
+        <th>Laulu nimi</th>
+        <th>Laulja</th>
+        <th>Pilt</th>
+        <th>Punktid</th>
+        <th>Lisamisaeg</th>
+        <th>+1 punkt</th>
+    </tr>
+    <?php
+    kuvaTabeliLaulud();
+    ?>
+</table>
+</body>
+</html>
