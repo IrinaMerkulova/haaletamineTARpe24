37d1f7256c9a9fa14d445151a431df4bac8ac0f6
Robin Tõll <Robintoll642@gmail.com>
2026-01-09 12:03:45 +0200

    valmis

--- conf.php
+++ conf.php
@@ -5,3 +5,10 @@ $parool='12345';
 $andmebaasinimi='haal';
 $yhendus=new mysqli($serverinimi, $kasutajanimi, $parool, $andmebaasinimi);
 $yhendus->set_charset("utf8");
+
+//$serverinimi='d141165.mysql.zonevs.eu';
+//$kasutajanimi='d141165_trobin';
+//$parool='12345';
+//$andmebaasinimi='haal';
+//$yhendus=new mysqli($serverinimi, $kasutajanimi, $parool, $andmebaasinimi);
+//$yhendus->set_charset("utf8");
