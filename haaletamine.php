91f884928d2125f9df6a16b687fa2cb31800f40d
Artur Sein <seinartur@gmail.com>
2026-01-09 09:57:54 +0200

    Kommentaaride lisamine

--- haaletamine.php
+++ haaletamine.php
@@ -13,6 +13,18 @@ if (isset($_REQUEST['lisa1punkt'])) {
     exit;
 }
 
+/* uue kommentaari lisamine */
+if (isset($_REQUEST['uus_kommentaar_id'])) {
+    $paring = $yhendus->prepare(
+        "UPDATE laulud SET kommentaarid = CONCAT(kommentaarid, ?) WHERE id = ?"
+    );
+    $komment2 = $_REQUEST['uus_kommentaar']."\n";
+    $paring->bind_param('si', $komment2, $_REQUEST['uus_kommentaar_id']);
+    $paring->execute();
+    header("Location: " . $_SERVER['PHP_SELF']);
+    exit;
+}
+
 /* Laulu lisamine */
 if (
     isset($_REQUEST['lauluNimi'], $_REQUEST['laulja']) &&
@@ -54,34 +66,45 @@ if (
 
 <table>
     <tr>
-        <th>Laulu nimi</th>
+        <th>Lisamisaeg</th>
         <th>Laulja</th>
+        <th>Laulu nimi</th>
         <th>Pilt</th>
         <th>Punktid</th>
-        <th>Lisamisaeg</th>
         <th>+1 punkt</th>
+        <th>Kommentaarid</th>
+        <th>Kommentaari lisamine</th>
     </tr>
 
 <?php
 $paring = $yhendus->prepare(
-    "SELECT id, lauluNimi, laulja, pilt, punktid, lisamisaeg, avalik
+    "SELECT id, lauluNimi, laulja, pilt, punktid, lisamisaeg, avalik, kommentaarid
      FROM laulud
      WHERE avalik = 1"
 );
 $paring->bind_result(
-    $id, $lauluNimi, $laulja, $pilt, $punktid, $lisamisaeg, $avalik
+    $id, $lauluNimi, $laulja, $pilt, $punktid, $lisamisaeg, $avalik, $kommentaarid
 );
 $paring->execute();
 
 while ($paring->fetch()) {
     echo "<tr>";
-    echo "<td>" . htmlspecialchars($lauluNimi) . "</td>";
+    echo "<td>$lisamisaeg</td>";
     echo "<td>" . htmlspecialchars($laulja) . "</td>";
+    echo "<td>" . htmlspecialchars($lauluNimi) . "</td>";
     echo "<td><img src='" . htmlspecialchars($pilt) . "'></td>";
     echo "<td>$punktid</td>";
-    echo "<td>$lisamisaeg</td>";
     echo "<td><a href='?lisa1punkt=$id'>+1 punkt</a></td>";
+    echo "<td>".nl2br(htmlspecialchars($kommentaarid))."</td>";
+    echo "<td>
+<form action='?' method='post'>
+<input type='hidden' name='uus_kommentaar_id' value='$id'>
+<input type='text' name='uus_kommentaar' id='uus_kommentaar'>
+<input type='submit' value='Ok'>
+</form>
+</td>";
     echo "</tr>";
+
 }
 ?>
 </table>
