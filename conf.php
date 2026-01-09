abe9b24663f9a6b7fa0a11641bd92972140fcaf5
Artur Sein <seinartur@gmail.com>
2026-01-09 09:09:32 +0200

    laulu peitmine

--- conf.php
+++ conf.php
@@ -1,7 +1,7 @@
 <?php
 $serverinimi='localhost';
-$kasutajanimi='opilaneStenver';
+$kasutajanimi='opilaneArtur';
 $parool='12345';
-$andmebaasinimi='';
+$andmebaasinimi='haal';
 $yhendus=new mysqli($serverinimi, $kasutajanimi, $parool, $andmebaasinimi);
 $yhendus->set_charset("utf8");
