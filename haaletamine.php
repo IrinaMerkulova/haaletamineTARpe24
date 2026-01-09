20719bf52ed1794ad83e9396b9143ab589584956
Irina Merkulova <irina.merkulova@gmail.com>
2026-01-09 10:57:46 +0200

    peitmine/naitmine on tehtud

--- haaletamine.php
+++ haaletamine.php
@@ -12,6 +12,17 @@ if (isset($_REQUEST['lisa1punkt'])) {
     header("Location: " . $_SERVER['PHP_SELF']);
     exit;
 }
+/* kommentaari lisamine */
+if (isset($_REQUEST['uus_kommentaar_id'])) {
+    $paring = $yhendus->prepare(
+        "UPDATE laulud SET kommentaarid=CONCAT(kommentaarid, ?) WHERE id = ?"
+    );
+    $komment2=$_REQUEST['uus_kommentaar']."\n";
+    $paring->bind_param('si', $komment2, $_REQUEST['uus_kommentaar_id']);
+    $paring->execute();
+    header("Location: " . $_SERVER['PHP_SELF']);
+    exit;
+}
 
 /* Laulu lisamine */
 if (
@@ -44,36 +55,52 @@ if (
 <body>
 
 <h1>🎵 Laulude hääletus</h1>
+<nav>
+    <ul>
+        <li><a href="haaletamine.php">Kasutaja leht</a></li>
+        <li><a href="haaletamineAdmin.php">Admin leht</a></li>
+    </ul>
+</nav>
 
 <table>
     <tr>
-        <th>Laulu nimi</th>
+        <th>Lisamisaeg</th>
         <th>Laulja</th>
+        <th>Laulunimi</th>
         <th>Pilt</th>
         <th>Punktid</th>
-        <th>Lisamisaeg</th>
         <th>+1 punkt</th>
+        <th>Kommentaarid</th>
+        <th>Kommentaari lisamine</th>
     </tr>
 
 <?php
 $paring = $yhendus->prepare(
-    "SELECT id, lauluNimi, laulja, pilt, punktid, lisamisaeg
+    "SELECT id, lauluNimi, laulja, pilt, punktid, lisamisaeg, kommentaarid
      FROM laulud
      WHERE avalik = 1"
 );
 $paring->bind_result(
-    $id, $lauluNimi, $laulja, $pilt, $punktid, $lisamisaeg
+    $id, $lauluNimi, $laulja, $pilt, $punktid, $lisamisaeg, $kommentaarid
 );
 $paring->execute();
 
 while ($paring->fetch()) {
     echo "<tr>";
-    echo "<td>" . htmlspecialchars($lauluNimi) . "</td>";
+    echo "<td>$lisamisaeg</td>";
     echo "<td>" . htmlspecialchars($laulja) . "</td>";
+    echo "<td>" . htmlspecialchars($lauluNimi) . "</td>";
     echo "<td><img src='" . htmlspecialchars($pilt) . "'></td>";
     echo "<td>$punktid</td>";
-    echo "<td>$lisamisaeg</td>";
     echo "<td><a href='?lisa1punkt=$id'>+1 punkt</a></td>";
+    echo "<td>".nl2br(htmlspecialchars($kommentaarid))."</td>";
+    echo "<td>
+<form action='?' method='post'>
+<input type='hidden' name='uus_kommentaar_id' value='$id'>
+<input type='text' name='uus_kommentaar' id='uus_kommentaar'>
+<input type='submit' value='OK'>
+</form>
+</td>";
     echo "</tr>";
 }
 ?>
@@ -95,3 +122,4 @@ while ($paring->fetch()) {
 
 </body>
 </html>
+
--- haaletamineAdmin.php
+++ haaletamineAdmin.php
@@ -22,6 +22,17 @@ if (isset($_REQUEST['peida_id'])) {
     header("Location: " . $_SERVER['PHP_SELF']);
     exit;
 }
+/* laulu näitamine */
+if (isset($_REQUEST['naita_id'])) {
+    $paring = $yhendus->prepare(
+        "UPDATE laulud SET avalik=1 WHERE id = ?"
+    );
+    $paring->bind_param('i', $_REQUEST['naita_id']);
+    $paring->execute();
+    header("Location: " . $_SERVER['PHP_SELF']);
+    exit;
+}
+
 
 /* Laulu lisamine */
 if (
@@ -54,6 +65,12 @@ if (
 <body>
 
 <h1>🎵 Laulude hääletus</h1>
+<nav>
+    <ul>
+        <li><a href="haaletamine.php">Kasutaja leht</a></li>
+        <li><a href="haaletamineAdmin.php">Admin leht</a></li>
+    </ul>
+</nav>
 
 <table>
     <tr>
@@ -63,16 +80,16 @@ if (
         <th>Punktid</th>
         <th>Lisamisaeg</th>
         <th>+1 punkt</th>
+        <th>Peida/Näita</th>
     </tr>
 
 <?php
 $paring = $yhendus->prepare(
-    "SELECT id, lauluNimi, laulja, pilt, punktid, lisamisaeg
-     FROM laulud
-     WHERE avalik = 1"
+    "SELECT id, lauluNimi, laulja, pilt, punktid, lisamisaeg, avalik
+     FROM laulud"
 );
 $paring->bind_result(
-    $id, $lauluNimi, $laulja, $pilt, $punktid, $lisamisaeg
+    $id, $lauluNimi, $laulja, $pilt, $punktid, $lisamisaeg, $avalik
 );
 $paring->execute();
 
@@ -84,6 +101,15 @@ while ($paring->fetch()) {
     echo "<td>$punktid</td>";
     echo "<td>$lisamisaeg</td>";
     echo "<td><a href='?lisa1punkt=$id'>+1 punkt</a></td>";
+    $tekst="Näita";
+    $seisund="naita_id";
+    $tekstlehel="Peidetud";
+    if($avalik==1){
+        $tekst="Peida";
+        $seisund="peida_id";
+        $tekstlehel="Nähtav";
+    }
+    echo "<td><a href='?$seisund=$id'>$tekst</a> ||| $tekstlehel</td>";
     echo "</tr>";
 }
 ?>
