a1e10ad06b1f68904ef1027aaf6bcbea57451c57
Robin Tõll <Robintoll642@gmail.com>
2026-01-09 09:33:56 +0200

    Peida, nähtav

--- haaletamineAdmin.php
+++ haaletamineAdmin.php
@@ -24,6 +24,17 @@ if (isset($_REQUEST['peida_id'])) {
     exit;
 }
 
+/* laulu näitamine*/
+if (isset($_REQUEST['naita_id'])) {
+    $paring = $yhendus->prepare(
+        "UPDATE laulud SET avalik=1 WHERE id = ?"
+    );
+    $paring->bind_param('i', $_REQUEST['naita_id']);
+    $paring->execute();
+    header("Location: " . $_SERVER['PHP_SELF']);
+    exit;
+}
+
 /* Laulu lisamine */
 if (
     isset($_REQUEST['lauluNimi'], $_REQUEST['laulja']) &&
@@ -55,7 +66,12 @@ if (
 <body>
 
 <h1>🎵 Laulude hääletus</h1>
-
+<nav>
+    <ul>
+        <li><a href="haaletamine.php">Kasutaja leht</a></li>
+        <li><a href="haaletamineAdmin.php">Admin leht</a></li>
+    </ul>
+</nav>
 <table>
     <tr>
         <th>Laulu nimi</th>
@@ -64,16 +80,16 @@ if (
         <th>Punktid</th>
         <th>Lisamisaeg</th>
         <th>+1 punkt</th>
+        <th>Peida/Näita</th>
     </tr>
 
 <?php
 $paring = $yhendus->prepare(
-    "SELECT id, lauluNimi, laulja, pilt, punktid, lisamisaeg
-     FROM laulud
-     WHERE avalik = 1"
+    "SELECT id, lauluNimi, laulja, pilt, punktid, lisamisaeg, avalik
+     FROM laulud"
 );
 $paring->bind_result(
-    $id, $lauluNimi, $laulja, $pilt, $punktid, $lisamisaeg
+    $id, $lauluNimi, $laulja, $pilt, $punktid, $lisamisaeg,$avalik
 );
 $paring->execute();
 
@@ -85,6 +101,15 @@ while ($paring->fetch()) {
     echo "<td>$punktid</td>";
     echo "<td>$lisamisaeg</td>";
     echo "<td><a href='?lisa1punkt=$id'>+1 punkt</a></td>";
+    $tekst="Näita";
+    $seisund="naita_id";
+    $tekstlehel="Peidetud";
+    if($avalik==1){
+        $tekst="Peida";
+        $seisund="peida_id";
+        $tekstlehel="Nähtav";
+    }
+    echo "</td><a href='?$seisund=$id'>$tekst</a>  ||| $tekstlehel</td>";
     echo "</tr>";
 }
 ?>
