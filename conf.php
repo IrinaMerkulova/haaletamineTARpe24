0ba0dd8f74bffd264b1c348b95bf7f0c83be5887
xZtsu <116342432+xZtsu@users.noreply.github.com>
2026-02-16 14:12:15 +0200

    git push projekti

--- conf.php
+++
@@ -1,7 +0,0 @@
-<?php
-$serverinimi='localhost';
-$kasutajanimi='opilaneAiron';
-$parool='12345';
-$andmebaasinimi='haal';
-$yhendus=new mysqli($serverinimi, $kasutajanimi, $parool, $andmebaasinimi);
-$yhendus->set_charset("utf8");
