a19161180f3802fa6afc2186e48459b61c752f97
Stenver <stenver.lindmaa@gmail.com>
2026-04-07 21:19:56 +0300

    Andmabaasi ühendus conf.php failis

--- conf.php
+++ conf.php
@@ -2,6 +2,12 @@
 $serverinimi='localhost';
 $kasutajanimi='opilaneStenver';
 $parool='12345';
-$andmebaasinimi='';
+$andmebaasinimi='haaletamine';
 $yhendus=new mysqli($serverinimi, $kasutajanimi, $parool, $andmebaasinimi);
+
+if ($yhendus->connect_error) {
+    die("Ühendus ebaõnnestus: " . $yhendus->connect_error);
+}
+
+
 $yhendus->set_charset("utf8");
